108d1b010d3e5fc60d1e0d3ac0f55182322e913d
Rina Kumari <rinas37@gmail.com>
2026-04-26 14:35:35 +0400

    Updated server file

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,25 @@ mongoose
 const app =express();
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
+/*app.use(cors({
     origin: process.env.CLIENT_URL,
     methods: ["GET","POST","DELETE","PUT"],
     allowedHeaders: ["Content-Type","Authorization","Cache-Control"],
      credentials: true,
+}));*/
+
+app.use(cors({
+    origin: (origin, callback) => {
+        const allowedOrigin = process.env.CLIENT_URL;
+        if (!origin || origin === allowedOrigin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
+    credentials: true,
 }));
 
 app.use(cookieParser());
